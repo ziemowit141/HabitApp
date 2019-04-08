63f69aefa8f2ccf1bd4890e04ccba143879c7dc1
TwojMistrz <ziemowit141>
2019-04-08 16:29:45 +0200

    First card in card View

--- app/src/main/java/com/example/habits/MainActivity.kt
+++ app/src/main/java/com/example/habits/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.habits
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iv_icon.setImageResource(R.drawable.water)
+        tv_title.text = getString(R.string.drink_water)
+        tv_description.text = getString(R.string.drink_water_description)
     }
 }
